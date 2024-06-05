8f4c465052c21050a02cf9e609db9327b6e2a727
Devansh Sharma <devsansh@yahoo.com>
2024-06-05 13:32:53 +0530

    Display and Append method added.

--- Array/arrayADT.cpp
+++ Array/arrayADT.cpp
@@ -1,33 +1,58 @@
 #include <iostream>
 using namespace std;
 
-class Array{
-    public:
+// Array Class with its variables and operations
+class Array {
+public:
     int *A;
     int size;
     int length;
+    
+    Array(int size,int length) {
+        this->size = size;
+        this->length = length;
+        A = new int[size];
+    }
+    
+    ~Array() {
+        delete[] A;
+    }
 
     void Display();
-
+    void Append(int number);
 };
 
-void Array::Display(){
-    for(int i = 0; i < size;i++){
-        cout<<A[i]<<" ";
+void Array::Display() {
+    for (int i = 0; i < length; i++) {
+        cout << A[i] << " ";
     }
-
+    cout << endl; // Add this line to move to the next line after displaying all elements
 }
-int main(){
-    Array arra = Array();
-    cout<<"Enter size of Array: ";
-    cin>>arra.size;
-    arra.A = new int[arra.size];
-    arra.length = 0;
-    for(int i = 0; i < arra.size;i++){
+
+void Array::Append(int number) {
+    if (length < size) { // Check to prevent out-of-bounds access
+        A[length] = number;
+        length++;
+    } else {
+        cout << "Array is full. Cannot append the number." << endl;
+    }
+}
+
+int main() {
+    int size,length;
+    cout << "Enter size of Array: ";
+    cin >> size;
+    cout<<"Number of elements to be filled: ";
+    cin>>length;
+    Array arra(size,length);
+    for (int i = 0; i < arra.length; i++) {
         arra.A[i] = rand();
     }
-    arra.length = arra.size;
-
+    // Display Array
     arra.Display();
+    // Example of appending a number
+    arra.Append(50);
+    arra.Display();
+    
     return 0;
 }
