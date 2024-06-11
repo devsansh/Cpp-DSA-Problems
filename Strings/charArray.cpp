bd36ddddfb68ca4aa3a3183961889b0168923fed
Devansh Sharma <devsansh@yahoo.com>
2024-06-11 18:48:26 +0530

    Declaration of character array.

--- Strings/charArray.cpp
+++ Strings/charArray.cpp
@@ -1,6 +1,17 @@
-#include <iostream>
-using namespace std;
-int main(){
-    char charArray[5] = {'a','b','c','d','e'};
+#include <iostream>  // Include the iostream library to use input and output stream
+using namespace std; // Use the standard namespace to avoid using the std:: prefix
+
+int main() {
+    // Declare and initialize a character array with 10 elements
+    // The array is partially filled with characters 'a', 'b', 'c', 'd', 'e' followed by a null terminator '\0'
+    // The rest of the elements will be automatically initialized to '\0' (null character)
+    char charArray[10] = {'a', 'b', 'c', 'd', 'e', '\0'};
+    
+    // Output the content of the character array to the console
+    // Since the array contains a null terminator, it will be treated as a C-style string
+    // The cout will print characters until it encounters the null terminator
+    cout << charArray;
+    
+    // Return 0 to indicate successful completion of the program
     return 0;
 }
