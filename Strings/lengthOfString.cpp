511156bd465a3c4750ce0742577eb1d82272f06c
Devansh Sharma <devsansh@yahoo.com>
2024-06-13 12:08:31 +0530

    To find length of a string

---
+++ Strings/lengthOfString.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char S[] = "Welcome,Devansh";
+    int i = 0;
+    while(S[i] != '\0'){
+        i++;
+    }
+    cout<<i<<endl;
+
+    return 0;
+}
