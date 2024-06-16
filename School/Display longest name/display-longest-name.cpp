e66416bedbd5b30c62413569bcbd693276eb7800
Devansh Sharma <122260173+devsansh@users.noreply.github.com>
2024-06-16 13:46:05 +0530

    Added Solution - GfG to GitHub

---
+++ School/Display longest name/display-longest-name.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cin >> A[i];
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i]<< " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    string longest(int n, const vector<string> &names) const {
+        if (names.empty()) {
+            return "";  // Handle the case where the input vector is empty
+        }
+        
+        string max_string = "";
+        for (const auto &name : names) {
+            if (max_string.length() < name.length()) {
+                max_string = name;
+            }
+        }
+        return max_string;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        
+        
+        vector<string> names(n);
+        Array::input(names,n);
+        
+        Solution obj;
+        string res = obj.longest(n, names);
+        
+        cout<<res<<"\n";
+        
+    }
+}
+
+// } Driver Code Ends
