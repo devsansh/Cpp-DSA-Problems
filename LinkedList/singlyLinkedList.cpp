7e258d2ff080b3eac324e6b8e40385f54156a258
Devansh Sharma <devsansh@yahoo.com>
2024-06-18 17:47:35 +0530

    Insertion completed.

--- LinkedList/singlyLinkedList.cpp
+++ LinkedList/singlyLinkedList.cpp
@@ -15,24 +15,36 @@ public:
 };
 
 // Function to insert a new node at the beginning of the list
-void insertAtHead(Node* &head, int data) {
+void insertAtHead(Node* &head, Node* &tail, int data) {
     Node* temp = new Node(data);  // Create a new node with given data
     temp->next = head;            // Set the new node's next pointer to the current head
     head = temp;                  // Update the head to the new node
+    if (tail == nullptr) {        // If the list was empty, update the tail as well
+        tail = temp;
+    }
+}
+
+// Function to insert a new node at the end of the list
+void insertAtTail(Node* &tail, int data) {
+    Node* newNode = new Node(data); // Create a new node with given data
+    if (tail != nullptr) {
+        tail->next = newNode;       // Link the new node to the current tail
+    }
+    tail = newNode;                 // Update the tail to the new node
 }
 
 // Function to insert a new node at the specified position in the list
-void insertAtPosition(Node* &head, int position, int data) {
-
-    if(position == 0){
-        insertAtHead(head,data);
+void insertAtPosition(Node* &head, Node* &tail, int position, int data) {
+    if (position == 0) {
+        insertAtHead(head, tail, data);
         return;
     }
+    
     Node* temp = head;
     int currentPosition = 0;
 
     // Traverse the list to the position before the insertion point
-    while(currentPosition < position - 1 && temp != nullptr) {
+    while (currentPosition < position - 1 && temp != nullptr) {
         temp = temp->next;
         currentPosition++;
     }
@@ -42,45 +54,71 @@ void insertAtPosition(Node* &head, int position, int data) {
         return;
     }
 
+    if (temp->next == nullptr) {
+        insertAtTail(tail, data);
+        return;
+    }
+
     // Create the new node and insert it into the list
     Node* newNode = new Node(data);
     newNode->next = temp->next;
     temp->next = newNode;
 }
 
-// Function to insert a new node at the end of the list
-void insertAtTail(Node* &tail, int data) {
-    Node* newNode = new Node(data); // Create a new node with given data
-    tail->next = newNode;           // Link the new node to the current tail
-    tail = newNode;                 // Update the tail to the new node
+// Function to calculate the length of the linked list
+int length(Node* head) {
+    int length = 0;                // Initialize length counter
+    Node* temp = head;             // Start from the head node
+    while (temp != nullptr) {      // Traverse the list until the end
+        length++;                  // Increment the length counter
+        temp = temp->next;         // Move to the next node
+    }
+    return length;                 // Return the total length
 }
 
 // Function to display the linked list
 void display(Node* head) {
-    Node* temp = head;            // Start from the head node
-    while (temp != nullptr) {     // Traverse the list until the end
+    Node* temp = head;             // Start from the head node
+    while (temp != nullptr) {      // Traverse the list until the end
         cout << temp->data << " -> ";  // Print the data of the current node
         temp = temp->next;             // Move to the next node
     }
-    cout << "nullptr" << endl;    // Indicate the end of the list
+    cout << "nullptr" << endl;     // Indicate the end of the list
 }
 
 int main() {
-    Node* head = NULL;
-    Node* tail = NULL;
-    Node* node1 = new Node(10);
+    Node* head = nullptr;
+    Node* tail = nullptr;
+    Node* node1 = new Node(10);   // Create the first node
     head = node1;
     tail = node1;
-/*------------------------------------------*/
     display(head);
-    insertAtHead(head,0);
+
+    insertAtTail(tail, 20);
     display(head);
-    insertAtTail(tail,30);
+
+    insertAtTail(tail, 30);
     display(head);
-    insertAtPosition(head,2,20);
+
+    insertAtTail(tail, 40);
     display(head);
-/*------------------------------------------*/
-    insertAtPosition(head,0,123);
+
+    insertAtTail(tail, 50);
     display(head);
+
+    insertAtTail(tail, 60);
+    display(head);
+
+    insertAtPosition(head, tail, 2, 70);
+    display(head);
+
+    insertAtPosition(head, tail, 0, 100);
+    display(head);
+
+    insertAtPosition(head, tail, 8, 999);
+    display(head);
+
+    cout << "Length of LinkedList: " << length(head) << endl;
+
     return 0;
 }
